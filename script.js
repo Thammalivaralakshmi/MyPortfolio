11b5def6b7a05f4da3f42127718c73a627ce09a5
Vara <thammalivaralakshmi273@gmail.com>
2025-07-03 18:04:15 +0530

    Portfolio Deployment

---
+++ script.js
@@ -0,0 +1,9 @@
+//for continuous typing
+
+var typed = new Typed("#element", {
+    strings: ["Software Developer","Java Developer", "Web Developer", "Software Engineer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+    showCursor: false,
+  });
